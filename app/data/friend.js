26bd9a95819c059efbf5f02076f7b5425f44c236
Louis Huang <jiannchyao@gmail.com>
2016-11-02 12:31:10 -0400

    version 1

---
+++ app/data/friend.js
@@ -0,0 +1,68 @@
+var friendList = [
+  {
+    name: 'Louis Huang',
+    profilePic: 'https://avatars1.githubusercontent.com/u/19935946?v=3&s=466',
+    scores: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    name: 'Barack Obama',
+    profilePic: ' https://pbs.twimg.com/profile_images/738744285101580288/OUoCVEXG.jpg',
+    scores: [
+      "4",
+      "4",
+      "3",
+      "3",
+      "3",
+      "2",
+      "3",
+      "3",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: 'Donald Trump',
+    profilePic: 'http://static2.politico.com/dims4/default/035efea/2147483647/resize/1160x%3E/quality/90/?url=http%3A%2F%2Fstatic.politico.com%2Fa2%2F01%2F4fa959254c2f9216910f47649004%2F160114-donald-trump-2-ap-1160.jpg',
+    scores: [
+      "4",
+      "3",
+      "3",
+      "4",
+      "5",
+      "5",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+  },
+  {
+    name: 'Hillary Clinton',
+    profilePic: 'http://static4.businessinsider.com/image/55fad9869dd7cc15008bb1ba-480/hillary-clinton-thumbs-up.jpg',
+    scores: [
+      "2",
+      "2",
+      "3",
+      "4",
+      "4",
+      "5",
+      "5",
+      "2",
+      "3",
+      "1"
+    ]
+  }
+]
+
+module.exports = friendList;
